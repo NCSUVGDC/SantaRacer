e04293abe3d7520b9893c451a66b67fbecd4f0c8
Aaron <aaronkingnc@att.net>
2020-12-17 02:45:47 -0500

    Added first person pov for krampus

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -5,46 +5,48 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class Catapult : MonoBehaviour
 {
-    public bool occupied = false;
-    public float angle = 45;
-    public float strength = 10f;
+    public bool occupied;
+    public float angle = 60;
+    public float strength = 1000f;
     PlayerControls control;
     GameObject player;
     bool inRange = false;
     public Camera krampuscamera;
+    Vector3 position;
     private void Start()
     {
+        occupied = false;
         control = new PlayerControls();
         control.Gameplay.Jump.performed += Launch;
         control.Gameplay.Jump.Enable();
         control.Gameplay.Interact.performed += Enter;
         control.Gameplay.Interact.Enable();
+        position = transform.position;
     }
     void Launch(CallbackContext ctx)
     {
         if (occupied)
         {
+            occupied = false;
             var dir = Quaternion.AngleAxis(angle, transform.right) * transform.forward;
             player.GetComponent<Rigidbody>().AddForce(dir * -strength);
-            player.transform.parent = null;
             player.GetComponent<KrampusMovement>().canjump = true;
             player.GetComponent<KrampusMovement>().movable = true;
-            krampuscamera.GetComponent<CameraMovement>().incatapult = false;
-            occupied = false;
+            //krampuscamera.GetComponent<Cam1stPerson>().inCatapult = false;
         }
     }
     void Enter(CallbackContext ctx)
     {
         if (inRange)
         {
-            player.transform.position = transform.position;
+            Vector3 newpos = transform.position;
+            newpos.y += 2;
+            player.transform.position = newpos;
             occupied = true;
-            player.transform.forward = -transform.forward;
-            player.transform.SetParent(transform);
             player.GetComponent<KrampusMovement>().movable = false;
             player.GetComponent<KrampusMovement>().canjump = false;
-            krampuscamera.GetComponent<CameraMovement>().incatapult = true;
-            krampuscamera.GetComponent<CameraMovement>().catapult = gameObject;
+            //krampuscamera.GetComponent<Cam1stPerson>().inCatapult = true;
+            //krampuscamera.GetComponent<Cam1stPerson>().catapult = transform;
         }
     }
     private void OnTriggerEnter(Collider other)
