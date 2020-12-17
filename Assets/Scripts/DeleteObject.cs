8ac72c2844f67a4f1ad7a7206018dad69b0fdc2a
Aaron <aaronkingnc@att.net>
2020-12-17 00:26:08 -0500

    Adding Krampus throwing Child
    Press Q

---
+++ Assets/Scripts/DeleteObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Krampus")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
