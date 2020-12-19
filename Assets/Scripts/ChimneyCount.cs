b0848e6c1817a2c2ea4ab754a495ea627d3dcfcf
Aaron <aaronkingnc@att.net>
2020-12-19 15:07:31 -0500

    Added chimneys and chimney manager

---
+++ Assets/Scripts/ChimneyCount.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimneyCount : MonoBehaviour
+{
+    public float used = 0;
+    float chimneycount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chimney[] children = GetComponentsInChildren<Chimney>();
+        chimneycount = children.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (used==chimneycount)
+        {
+            //end game
+        }
+    }
+}
